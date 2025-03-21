9e64667dcadbad02aef8eaf79d157ef2c062341b
Александр Григоров <grigorovalex30@yandex.ru>
2025-03-21 19:58:43 +0300

    21/03/2025 19:58

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
 from django.http import Http404
+from django.shortcuts import render
 
 posts = [
     {
@@ -31,7 +31,6 @@ posts = [
                 гиблого места.''',
     },
     {
-        
         'id': 2,
         'location': 'Остров отчаянья',
         'date': '25 октября 1659 года',
